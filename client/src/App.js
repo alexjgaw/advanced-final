a2cbe725ae8cef0a1f9377930068e615bc0ea459
Alex Gaw <alexanderjgaw@gmail.com>
2017-03-26 19:48:55 -0500

    Started styling nav bar. Bg color, borders, starting to work with Affix

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className="App">
+        <Footer />
         <TopNavbar />
         <Hero />
         <Explainer />
--- client/src/components/TopNavbar/index.js
+++ client/src/components/TopNavbar/index.js
@@ -1,31 +1,33 @@
+// TODO: Work on affix styling because it's all kinds of fucked up. But it doesn't break the page!
+
 import React, { PropTypes } from 'react';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Affix } from 'react-overlays';
 import { Link } from 'react-router-dom';
 import Logo from '../Logo/index';
 import './index.css';
 
+
 const TopNavbar = (props) => {
   return (
-    <Navbar inverse collapseOnSelect className="eff-that-nav">
-      <Navbar.Header>
-        <Navbar.Brand>
-          <Logo />
-        </Navbar.Brand>
-        { props.showNavItems ? <Navbar.Toggle /> : null }
-      </Navbar.Header>
-      {
-        props.showNavItems ?
-          <Navbar.Collapse>
-            <Nav pullRight>
-              <NavItem onClick={props.onSignOut}>Sign Out</NavItem>
-            </Nav>
-            <Nav pullRight>
-              <Link to="/secret"><Navbar.Text>Secret</Navbar.Text></Link>
-            </Nav>
-          </Navbar.Collapse> :
-          null
-      }
-    </Navbar>
+    <Affix>
+      <Navbar inverse collapseOnSelect id="eff-that-nav">
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Logo />
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <NavItem onClick={props.onSignOut}>Sign Out</NavItem>
+          </Nav>
+          <Nav pullRight>
+            <Link to="/secret"><Navbar.Text>Secret</Navbar.Text></Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Affix>
   );
 };
 
