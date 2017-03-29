f5d92486f605cc2cb10b321ce61aa0132233cdf1
Alex Gaw <alexanderjgaw@gmail.com>
2017-03-29 15:52:21 -0500

    Added some text to explainer

--- client/src/components/Explainer/index.js
+++ client/src/components/Explainer/index.js
@@ -10,9 +10,17 @@ const Explainer = () => {
         <Row>
           <Col md={6} mdOffset={3}>
             <h1>What is it?</h1>
-            <p><Logo /> is a micro-donation platform built
-            to help ordinary people stand up to the President of the United
-            States and his cronies.</p>
+            <p><Logo /> is a mini-donation platform built
+            to help ordinary people stand up for American values in the
+            face of ignorant and dangerous leadership.</p>
+            <h2>How does it work?</h2>
+            <p>
+              Lucas ipsum dolor sit amet r2-d2 skywalker kashyyyk mace
+              coruscant dantooine mon qui-gonn skywalker jinn. Hutt
+              dagobah organa moff gamorrean. Jabba gonk skywalker hutt.
+              Organa dooku c-3po kessel moff hoth luke cade yoda. Wedge
+              fett hutt zabrak. Grievous organa moff antilles.
+            </p>
           </Col>
         </Row>
       </Grid>
