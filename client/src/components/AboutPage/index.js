c397dd59493e5c773bb32b796e60758699fb672b
Alex Gaw <alexanderjgaw@gmail.com>
2017-04-14 15:25:44 -0500

    continue work on AboutPage

--- client/src/components/AboutPage/index.js
+++ client/src/components/AboutPage/index.js
@@ -9,7 +9,7 @@ const AboutPage = () => {
         <Row>
           <Col md={6}>
             <h1>About the Project</h1>
-            <p>
+            <p className="justify-text">
               <Logo /> began as the final project for the Advanced course at Austin Coding Academy.
               I was talking to my instructor Ivo about Trump's latest tweet. I don't remember
               specifically what it was, but it's safe to assume it was horrible. And I said
@@ -24,6 +24,38 @@ const AboutPage = () => {
             </p>
             <p>
               Then I said, "Okay!"
+            </p>
+            <p className="justify-text">
+            As you can tell, I tend to choose ambitious projects when I'm just getting started.
+            I also prefer to choose projects that align with my values. This is a
+            great example. I'm passionate about social-justice issues and thought
+            there should be an easy, public way for people to engage with the president
+            (and those associated with him) on the issues they care about. Not everybody
+            has the time/energy for phone calls, letters, rallies, or thinkpieces. Most
+            people don't have the extra money to donate large sums to causes
+            they otherwise support. <strong>But!</strong> Many people can afford to donate
+            small amounts. That's where <Logo /> comes in. Each individual contribution
+            might not be much, but together maybe we can make a real difference.
+            Either the people in power will fund organizations they oppose, or they'll
+            stop spewing vitriol about them. Either way it's a win.
+            </p>
+          </Col>
+          <Col md={6}>
+            <img src="http://placehold.it/300x250" alt="placeholder" />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6}>
+            <img src="http://placehold.it/300x250" alt="placeholder" />
+          </Col>
+          <Col md={6}>
+            <h1>About the Developer</h1>
+            <p className="justify-text">
+              My name is Alex Gaw. I'm a recent graduate of Austin Coding Academy,
+              a full-stack web development program here in Austin, Texas.
+            </p>
+            <p>
+
             </p>
           </Col>
         </Row>
